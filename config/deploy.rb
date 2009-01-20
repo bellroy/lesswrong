7c673e628c8ee5e065ef0b93f8f47bbd837af821
toby <toby@heidi.tricycledevelopments.com>
2009-01-20 14:47:41 +1100

    Capified

---
+++ config/deploy.rb
@@ -0,0 +1,37 @@
+stages_glob = File.join(File.dirname(__FILE__), "deploy", "*")
+stages = Dir[stages_glob].collect {|f| File.basename(f) }.sort
+set :stages, stages
+
+require 'capistrano/ext/multistage'
+load 'config/cap-tasks/trike-tasks.rb'
+
+set :scm, 'git'
+set :repository, "git@github.com:tricycle/lesswrong.git"
+set :git_enable_submodules, 1
+set :deploy_via, :remote_cache
+set :repository_cache, 'cached-copy'
+
+# Be sure to change these in your application-specific files
+set :branch, 'master'
+
+set :user, "www-data"            # defaults to the currently logged in user
+default_run_options[:pty] = true
+
+namespace :deploy do
+  task :after_update_code, :roles => [:web, :app] do
+    %w[files assets].each {|dir| link_shared_dir(dir) }
+  end
+
+  def link_shared_dir(dir)
+    shared_dir = "#{deploy_to}/shared/#{dir}"
+    public_dir = "#{release_path}/public/#{dir}"
+    run "mkdir -p #{shared_dir}"  # make sure the shared dir exists
+    run "if [ -e #{public_dir} ]; then rm -rf #{public_dir} && echo '***\n*** #{public_dir} removed (in favour of a symlink to the shared version) ***\n***'; fi"
+    run "ln -sv #{shared_dir} #{public_dir}"
+  end
+end
+
+#before 'deploy:update_code', 'git:ensure_pushed'
+#before 'deploy:update_code', 'git:ensure_deploy_branch'
+#after "deploy:update_code", "deploy:symlink_remote_db_yaml"
+#after "deploy:symlink", "deploy:apache:config"
