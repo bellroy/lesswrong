ffaf8269a72cd503a85e8dc8ce3c092e708522a0
Wesley Moore <wes@trike.com.au>
2009-02-04 14:20:22 +1100

    Enable inline popups

--- r2/r2/public/static/tinymce.js
+++ r2/r2/public/static/tinymce.js
@@ -10,8 +10,8 @@ function init_tinymce() {
     theme_advanced_buttons1 : "formatselect,|,bold,italic,underline,strikethrough,blockquote,summarybreak,|,bullist,numlist,|,outdent,indent,|,link,unlink,anchor,image,code,html,|,hr,removeformat,|,sub,sup,|,charmap",
     theme_advanced_buttons2 : "",
     theme_advanced_buttons3 : "",
-    plugins : "summarybreak,advimage", //inlinepopups,
-    file_browser_callback : 'showImageBrowser'
+    plugins : "summarybreak,inlinepopups,advimage",
+    file_browser_callback : 'showImageBrowser',
   }); 
 };
 
