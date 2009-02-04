ceff49d2007bfa39d645005ec43c5ca5f0b211f1
Wesley Moore <wes@trike.com.au>
2009-02-05 10:28:07 +1100

    Change name of DefaultSR to default_sr in config
    The name is used to build URLs so needs to be the proper name of the Subreddit

--- r2/r2/models/subreddit.py
+++ r2/r2/models/subreddit.py
@@ -478,7 +478,7 @@ class AllSR(FakeSubreddit):
 
 class DefaultSR(FakeSubreddit):
     #notice the space before reddit.com
-    name = g.front_page_title
+    name = g.default_sr
     path = '/'
     header = 'http://static.reddit.com/reddit.com.header.png'
 
