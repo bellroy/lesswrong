0947d154c6c338e17e0502fa152d83df2c4fdf9a
Wesley Moore <wes@trike.com.au>
2009-02-16 16:23:19 +1100

    Encode tag paths as utf 8
    Fixes Issue #55

--- r2/r2/models/link.py
+++ r2/r2/models/link.py
@@ -599,7 +599,7 @@ class Tag(Thing):
     @property
     def path(self):
         """Returns the path to the tag listing for this tag"""
-        return "/tag/%s/" % (urllib.quote(self.name))
+        return "/tag/%s/" % (urllib.quote(self.name.encode('utf8')))
 
 class LinkTag(Relation(Link, Tag)):
     pass
