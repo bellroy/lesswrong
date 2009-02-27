7f2c6eee0b3d6bec0065680eebd96649c0166981
Wesley Moore <wes@trike.com.au>
2009-02-27 14:31:47 +1100

    Handle utf8 tags that come in as ASCII when generating the tag cloud

--- r2/r2/models/link.py
+++ r2/r2/models/link.py
@@ -31,6 +31,7 @@ from r2.lib import utils
 from mako.filters import url_escape
 from r2.lib.strings import strings, Score
 from r2.lib.db.operators import lower
+from r2.lib.filters import _force_unicode
 
 from pylons import c, g, request
 from pylons.i18n import ungettext
@@ -625,6 +626,7 @@ class Tag(Thing):
 
         link_type = tdb.types_id[Link._type_id]
         link_data_table = link_type.data_table[0]
+        link_thing_table = link_type.thing_table
 
         link_sr = sa.select([
             link_data_table.c.thing_id,
@@ -634,9 +636,13 @@ class Tag(Thing):
         query = sa.select([linktag_thing_table.c.thing2_id,
                           sa.func.count(linktag_thing_table.c.thing1_id)],
                           sa.and_(linktag_thing_table.c.thing1_id == link_sr.c.thing_id,
+                                  linktag_thing_table.c.thing1_id == link_thing_table.c.thing_id,
+                                  link_thing_table.c.spam == False,
                                   link_sr.c.sr_id.in_(*sr_ids)),
                           group_by = [linktag_thing_table.c.thing2_id],
-                          having = sa.func.count(linktag_thing_table.c.thing1_id) > 0)
+                          having = sa.func.count(linktag_thing_table.c.thing1_id) > 0,
+                          order_by = sa.desc(sa.func.count(linktag_thing_table.c.thing1_id)),
+                          limit = 100)
 
         rows = query.execute().fetchall()
         tags = []
@@ -645,7 +651,7 @@ class Tag(Thing):
             tags.append((tag, result.count))
 
         # Order by tag name
-        tags.sort(key=lambda x: x[0].name)
+        tags.sort(key=lambda x: _force_unicode(x[0].name))
         return cls.make_cloud(10, tags)
 
     @classmethod
