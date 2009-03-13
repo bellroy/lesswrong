bbaffecc401c1e765764ccc03c58cf78bb41e30a
Wesley Moore <wes@trike.com.au>
2009-03-13 15:01:53 +1100

    Make test run without full Pylons stack

--- r2/r2/tests/unit/test_link.py
+++ r2/r2/tests/unit/test_link.py
@@ -1,20 +1,44 @@
-from r2.tests import *
+import r2.tests
+import nose
+from paste.registry import Registry
+from r2.lib.app_globals import Globals
+from paste.deploy import appconfig
 
-class TestLink(TestController):
+# from r2.config.environment import load_environment
+from r2.templates import tmpl_dirs
+import os
+import pylons
+from pylons.i18n.translation import NullTranslations
+
+
+class TestLink(object):
     
-    def setUp(self):
-        response = self.app.get('/') # Hack that sets up the global environment
+    def setup(self):
+        # response = self.app.get('/') # Hack that sets up the global environment
+        config = appconfig('config:test.ini', relative_to=r2.tests.conf_dir)
+        root_path = os.path.join(r2.tests.conf_dir, 'r2')
+        paths = {'root': root_path,
+                 'controllers': os.path.join(root_path, 'controllers'),
+                 'templates': tmpl_dirs,
+                 'static_files': os.path.join(root_path, 'public')
+                 }
+
+        self.registry = Registry()
+        self.registry.prepare()
+        self.globals = Globals(config.global_conf, config.local_conf, paths)
+        self.registry.register(pylons.g, self.globals)
+
+        self.registry.register(pylons.translator, NullTranslations())
+
         # Create a link here
-        from r2.models import Link  
         self.link = { 'name': 'Link Name' }
-        pass
+        from r2.models import Link  
     
-    def tearDown(self):
+    def teardown(self):
         # drop Link here
         self.link = None
-        pass
     
     def test_name(self):
         print self.link
-        assert self.link['name'] == 'Link Nane'
+        assert self.link['name'] == 'Link Name'
 
