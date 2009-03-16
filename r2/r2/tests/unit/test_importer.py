536f652095d75cee869f42fa4d06746093dbf216
Wesley Moore <wes@trike.com.au>
2009-03-16 14:34:51 +1100

    Add stub importer test

---
+++ r2/r2/tests/unit/test_importer.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import nose
+
+from r2.lib import importer
+
+class TestAtomImporter(object):
+
+    def test_comments_on_post(self):
+        pass
+
+    def test_posts_by_author(self):
+        pass
+
+    def test_rewrite_urls_in_post_body(self):
+        pass
+    
+    def test_rewrite_urls_in_comments(self):
+        pass
+
+    def test_set_sort_order(self):
+        pass
+    
+    def test_filter_html_in_titles(self):
+        pass
+    
+    def test_set_comment_is_html(self):
+        pass
+    
+    def test_auto_account_creation(self):
+        pass
