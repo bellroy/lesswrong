6b8b31302994edfb8132b831987c662621bc169d
Wesley Moore <wes@trike.com.au>
2009-03-23 15:48:31 +1100

    The gdata module has changed names

--- r2/setup.py
+++ r2/setup.py
@@ -88,7 +88,7 @@ setup(
                       "psycopg2",
                       "py_interface",
                       "lxml>=2",
-                      "gdata.py>=1.2.4",], # For Atom parsing
+                      "gdata>=1.2.4",], # For Atom parsing
     packages=find_packages(),
     include_package_data=True,
     test_suite = 'nose.collector',
