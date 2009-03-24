8f77f4efc6bf715b3589d6d9ec2bec04671d64ff
Wesley Moore <wes@trike.com.au>
2009-03-24 13:59:25 +1100

    Finally runs to completion
    Adds permalinks and updates content, filters out non-Yudkowsky posts

--- scripts/add_links_to_ob_export.py
+++ scripts/add_links_to_ob_export.py
@@ -4,8 +4,15 @@ import yaml
 import re
 
 kill_whitespace_re = re.compile('\s')
+kill_entities_re = re.compile('&#?[a-z0-9]{1,4};')
 def kill_whitespace(body):
-    return kill_whitespace_re.sub('', body)
+    body = kill_whitespace_re.sub('', body)
+    body = kill_entities_re.sub('', body)
+    body = body.replace('<p>', '')
+    body = body.replace('</p>', '')
+    body = body.replace('<br/>', '')
+    
+    return body
 
 if __name__ == '__main__':
     if len(sys.argv) <= 3:
@@ -18,15 +25,13 @@ if __name__ == '__main__':
     export_file = open(sys.argv[1])
     api_file = open(sys.argv[2])
     output_file = open(sys.argv[3], 'w')
-    mappings = yaml.load(api_file)
-    export   = yaml.load(export_file)
+    mappings = yaml.load(api_file, Loader=yaml.CLoader)
+    export   = yaml.load(export_file, Loader=yaml.CLoader)
 
     # Turn the mappings into a lookup table on title and content
     post_mapping = {}
     title_mapping = {}
     for post in mappings:
-        print post['title']
-        
         title = post['title']
         body = post['description'] + post['mt_text_more']
         if not isinstance(body, unicode):
@@ -36,30 +41,44 @@ if __name__ == '__main__':
 
         key = (kill_whitespace(body), kill_whitespace(title))
         post_mapping[key] = post
-        title_mapping[title] = key
+        title_mapping[kill_whitespace(title)] = key
 
     # Scan the export file
+    new_export = []
     for entry in export:
-        # Get the date and title and do a lookup on the permalink
+        if 'Eliezer Yudkowsky' not in entry['author']:
+            continue
+
+        # Get the title and do a lookup on the permalink
         body = entry['description'] + entry['mt_text_more']
         body = body.decode('utf-8')
         title = entry['title']
         title = title.decode('utf-8')
+        print title
+        # flush(sys.stdout)
 
         key = (kill_whitespace(body), kill_whitespace(title))
         try:
             api_post = post_mapping[key]
         except KeyError:
-            print title_mapping[entry['title']]
+            print title_mapping[kill_whitespace(title)]
             print 
             print key
             print
-            raise
             
-        entry['permalink'] = api_post['permaLink']
-        entry['description'] = api_post['description']
-        entry['mt_text_more'] = api_post['mt_text_more']
+            import difflib
+            d = difflib.Differ()
+            diff = d.compare(title_mapping[kill_whitespace(title)], key)
+            import pprint
+            pprint.pprint(list(diff))
+            raise
+        
+        new_entry = entry
+        new_entry['permalink'] = api_post['permaLink']
+        new_entry['description'] = api_post['description']
+        new_entry['mt_text_more'] = api_post['mt_text_more']
+        new_export.append(new_entry)
     
     # Print out the result
-    yaml.dump(export, output_file)
+    yaml.dump(new_export, output_file, Dumper=yaml.CDumper)
         
