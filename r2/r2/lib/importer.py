3ed71d015e1b790a5ec0467265d41250d3fbedfd
Darius Powell <dariusp@trike.com.au>
2009-03-25 13:18:47 +1100

    Reworked Importer and associated unit tests from an Atom feed to a YAML based data structure.

--- r2/r2/lib/importer.py
+++ r2/r2/lib/importer.py
@@ -1,5 +1,3 @@
-from gdata import atom
-
 import sys
 import os
 import re
@@ -12,18 +10,6 @@ from r2.lib.db.thing import NotFound
 ###########################
 # Constants
 ###########################
-
-#BLOGGER_URL = 'http://www.blogger.com/'
-#BLOGGER_NS = 'http://www.blogger.com/atom/ns#'
-KIND_SCHEME = 'http://schemas.google.com/g/2005#kind'
-
-
-#YOUTUBE_RE = re.compile('http://www.youtube.com/v/([^&]+)&?.*')
-#YOUTUBE_FMT = r'[youtube=http://www.youtube.com/watch?v=\1]'
-#GOOGLEVIDEO_RE = re.compile('(http://video.google.com/googleplayer.swf.*)')
-#GOOGLEVIDEO_FMT = r'[googlevideo=\1]'
-#DAILYMOTION_RE = re.compile('http://www.dailymotion.com/swf/(.*)')
-#DAILYMOTION_FMT = r'[dailymotion id=\1]'
 
 MAX_RETRIES = 10
 
@@ -43,100 +29,50 @@ def generate_password():
         password.append(rng.choice(ALL_PASSWORD_CHARS))
     return ''.join(password)
 
-class AtomImporter(object):
+class Importer(object):
 
-    def __init__(self, doc, url_handler=None):
-        """Constructs an importer for an Atom (aka Blogger export) file.
+    def __init__(self, url_handler=None):
+        """Constructs an importer that takes a data structure based on a yaml file.
 
         Args:
-        doc: The Atom file as a string
-        url_handler: A optional URL transformation function that will be 
+        url_handler: A optional URL transformation function that will be
         called with urls detected in post and comment bodies.
         """
 
-        # Ensure UTF8 chars get through correctly by ensuring we have a
-        # compliant UTF8 input doc.
-        self.doc = doc.decode('utf-8', 'replace').encode('utf-8')
-
-        # Read the incoming document as a GData Atom feed.
-        self.feed = atom.FeedFromString(self.doc)
-        
-        # Generate a list of all the posts and their comments
-        self.posts = {}
-        # Stores the id of posts in the order they appear in the feed.
-        self.post_order = []
-        self.comments = {}
         self.url_handler = url_handler if url_handler else self._default_url_handler
+
         self.username_mapping = {}
-        
-        self._scan_feed()
 
     @staticmethod
     def _default_url_handler(match):
         return match.group()
 
-    def _scan_feed(self):
-        for entry in self.feed.entry:
-            # Grab the information about the entry kind
-            entry_kind = ""
-            for category in entry.category:
-                if category.scheme == KIND_SCHEME:
-                    entry_kind = category.term
-
-                    if entry_kind.endswith("#comment"):
-                        # This entry will be a comment, grab the post that it goes to
-                        in_reply_to = entry.FindExtensions('in-reply-to')
-                        post_id = in_reply_to[0].attributes['ref'] if in_reply_to else None
-                        self._rewrite_urls(entry)
-                        comments = self.comments.setdefault(post_id, [])
-                        comments.append(entry)
-
-                    elif entry_kind.endswith("#post"):
-                        # This entry will be a post
-                        # posts_map[self._ParsePostId(entry.id.text)] = post_item
-                        post_id = entry.id.text
-                        self._rewrite_urls(entry)
-                        self.post_order.append(post_id) # Assumes a post has a unique id
-                        self.posts[post_id] = entry
-
-
     # Borrowed from http://stackoverflow.com/questions/161738/what-is-the-best-regular-expression-to-check-if-a-string-is-a-valid-url/163684#163684
-    url_re = re.compile(r"""(?:https?|ftp|file)://[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|]""", re.IGNORECASE)
-    def _rewrite_urls(self, entry):
-        if not entry.content.text:
-            return
-        entry.content.text = self.url_re.sub(self.url_handler, entry.content.text)
+#     url_re = re.compile(r"""(?:https?|ftp|file)://[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|]""", re.IGNORECASE)
+#     def _rewrite_urls(self, entry):
+#         if not entry.content.text:
+#             return
+#         entry.content.text = self.url_re.sub(self.url_handler, entry.content.text)
 
-    def overcomingbias_rewiter(self, match):
-        """Rewrite overcoming bias URLs to LessWrong ones"""
-        return
+    def process_post(self, post):
+        account = self._get_or_create_account(post['author'], post['authorEmail'])
+        # TODO: do more...
 
-    def get_post(self, post_id):
-        """Retrieve a post by its unique id"""
-        return self.posts[post_id]
+    def import_into_subreddit(self, sr, data):
+        for post in data:
+            try:
+                self.process_post(post)
+            except Exception, e:
+                # TODO: do more...
+                pass
 
-    def posts_by(self, authors):
-        for post_id in self.post_order:
-            entry = self.posts[post_id]
-            for author in entry.author:
-                if author.name.text in authors:
-                    yield entry
-                    break
-
-    def comments_on_post(self, post_id):
-        return self.comments.get(post_id)
-
-    def import_into_subreddit(self, sr):
-        for post_id in self.post_order:
-            post = self.posts[post_id]
-            
-            # Get the account for this post
-            author = post.author[0]
-            raise NotImplementedError
+    def _username_from_name(self, name):
+        """Convert a name into a username"""
+        return name.replace(' ', '_')
 
     def _find_account_for(self, name, email):
         """Try to find an existing account using derivations of the name"""
-        
+
         try:
             account = self.username_mapping[(name, email)]
         except KeyError:
@@ -145,7 +81,7 @@ class AtomImporter(object):
                 name.replace(' ', ''),
                 self._username_from_name(name),
             )
-        
+
             account = None
             for candidate in candidates:
                 q = Account._query(
@@ -165,10 +101,6 @@ class AtomImporter(object):
             raise NotFound
 
         return account
-
-    def _username_from_name(self, name):
-        """Convert a name into a username"""
-        return name.replace(' ', '_')
 
     def _get_or_create_account(self, name, email):
         try:
@@ -192,33 +124,3 @@ class AtomImporter(object):
                     break
 
         return account
-
-########## Mostly ad hoc, investigative stuff from here on
-
-    def show_posts_by(self, authors):
-        """Print the titles of the posts by the list of supplied authors"""
-        for entry in self.posts_by(authors):
-            # print '%s by %s' % (entry.title.text, author.name.text)
-            print entry.title.text
-
-    def show_users_with_email(self):
-        for comments in self.comments.itervalues():
-            for comment in comments:
-                author = comment.author[0]
-                if author.email:
-                    print "%s|%s" % (author.name.text, author.email.text)
-
-if __name__ == '__main__':
-    if len(sys.argv) <= 1:
-        print 'Usage: %s <blogger_export_file>' % os.path.basename(sys.argv[0])
-        print
-        print ' Imports the blogger export file.'
-        sys.exit(-1)
-
-    xml_file = open(sys.argv[1])
-    xml_doc = xml_file.read()
-    xml_file.close()
-    importer = AtomImporter(xml_doc)
-    xml_doc = None
-    #print importer.show_posts_by('Eliezer Yudkowsky')
-    importer.show_users_with_email()
--- r2/r2/tests/unit/test_importer.py
+++ r2/r2/tests/unit/test_importer.py
@@ -6,89 +6,68 @@ from r2.tests import ModelTest
 from r2.lib.db.thing import NotFound
 
 import r2.lib.importer
-from r2.lib.importer import AtomImporter
+from r2.lib.importer import Importer
 
 import re
-import gdata
-from gdata import atom
+import datetime
 
-DUMMY_EMAIL = 'test@lesswrong.com'
-DUMMY_URI = 'http://www.lesswrong.com/'
-ATOM_TYPE = 'application/atom+xml'
-HTML_TYPE = 'text/html'
-CATEGORY_NS = 'http://www.blogger.com/atom/ns#'
-CATEGORY_KIND = 'http://schemas.google.com/g/2005#kind'
-POST_KIND = 'http://schemas.google.com/blogger/2008/kind#post'
-COMMENT_KIND = 'http://schemas.google.com/blogger/2008/kind#comment'
-ATOM_THREADING_NS = 'http://purl.org/syndication/thread/1.0'
+class ImporterFixture(object):
 
-class InReplyTo(atom.ExtensionElement):
-  """Supplies the in-reply-to element from the Atom threading protocol."""
+    def __init__(self):
+        self.ids = {}
+        self.data = []
+        self.posts_by_id = {}
 
-  def __init__(self, ref, href=None):
-    """Constructs an InReplyTo element."""
-    attrs = {}
-    attrs['ref'] = ref
-    attrs['type'] = ATOM_TYPE
-    if href:
-      attrs['href'] = href
-    atom.ExtensionElement.__init__(self, 'in-reply-to',
-                                   namespace=ATOM_THREADING_NS,
-                                   attributes=attrs)
-
-class AtomFeedFixture(object):
-
-    def __init__(self, title='Feed Fixture', url='http://www.lesswrong.com/.rss', alternate_url='http://www.lesswrong.com/'):
-        # Create the top-level feed object
-        feed = gdata.GDataFeed()
-
-        # Fill in the feed object with the boilerplate metadata
-        feed.generator = atom.Generator(text='Blogger')
-        feed.title = atom.Title(text=title)
-        feed.link.append(
-            atom.Link(href=url, rel='self', link_type=ATOM_TYPE))
-        feed.link.append(
-            atom.Link(href=alternate_url, rel='alternate', link_type=HTML_TYPE))
-        self.feed = feed
-        self.ids  = {}
-
-    #TODO Need an author generator: name, email
-    def _add_entry(self, entry_id, kind, author='Anonymous', content='', **kw):
-        entry = gdata.GDataEntry()
-        entry.link.append(
-            atom.Link(href=DUMMY_URI, rel='self', link_type=ATOM_TYPE))
-        entry.link.append(
-            atom.Link(href=DUMMY_URI, rel='alternate', link_type=HTML_TYPE))
-
-        # Add the author
-        #author_name = self._Encode(value)
-        entry.author = atom.Author(atom.Name(text=author))
-
-        # Add the content
-        entry.content = atom.Content(
-            content_type='html', text=content)
-            # content_type='html', text=self._TranslateContents(tag_contents))
-
-        # Add the kind
-        entry.category.append(
-            atom.Category(scheme=CATEGORY_KIND, term=kind))
-
-        if kind == COMMENT_KIND:
-            # Add the post id to the comment
-            entry.extension_elements.append(InReplyTo(kw['post_id']))
-
-        # Add the id of the entry
-        entry.id = atom.Id(entry_id)
-        self.feed.entry.append(entry)
-        return entry_id
-
-    def add_post(self, **kw):
+    def add_post(self,
+                 author='Anonymous',
+                 author_email='anon@nowhere.org',
+                 category='Category',
+                 title='Title',
+                 description='',
+                 mt_text_more='',
+                 mt_keywords='',
+                 status='Publish',
+                 date_created=None,
+                 permalink=None):
         post_id = self.post_id
-        return self._add_entry(post_id, POST_KIND, **kw)
+        if not date_created:
+            date_created = datetime.datetime.now().strftime('%m/%d/%Y %r')
+        if not permalink:
+            permalink = 'http://www.overcomingbias.com/%s.html' % post_id
+        post = { 'postid': post_id,
+                 'author': author,
+                 'authorEmail': author_email,
+                 'category': category,
+                 'title': title,
+                 'description': description,
+                 'mt_text_more': mt_text_more,
+                 'mt_keywords': mt_keywords,
+                 'status': status,
+                 'dateCreated': date_created,
+                 'permaLink': permalink }
+        self.data.append(post)
+        self.posts_by_id[post_id] = post
+        return post_id
 
-    def add_comment(self, post_id, **kw):
-        comment_id = self.comment_id
-        return self._add_entry(comment_id, COMMENT_KIND, post_id=post_id, **kw)
+    def add_comment(self,
+                    post_id,
+                    author='Anonymous',
+                    author_email='anon@nowhere.org',
+                    author_url='',
+                    body='',
+                    date_created=None):
+      if not date_created:
+          date_created = datetime.datetime.now().strftime('%m/%d/%Y %r')
+
+          comment = { 'author': author,
+                      'authorEmail': author_email,
+                      'authorUrl': author_url,
+                      'body': body,
+                      'dateCreated': date_created }
+
+          self.posts_by_id[post_id].setdefault('comments', []).append(comment)
+
+          return comment
 
     def __getattr__(self, attr):
         if attr.endswith('_id'):
@@ -97,41 +76,10 @@ class AtomFeedFixture(object):
         else:
             raise AttributeError, '%s not found' % attr
 
-    def __str__(self):
-        # Return self an an XML atom feed string
-        # print str(self.feed)
-        return str(self.feed)
+    def get_data(self):
+        return self.data
 
-class TestAtomImporter(object):
-
-    def test_posts_by_author(self):
-        feed = AtomFeedFixture()
-        for author in ['Joe Bloggs','Not Joe Bloggs','Joe Bloggs','Someone Else']:
-            feed.add_post(author=author)
-        importer = AtomImporter(str(feed))
-        post_ids = [post.id.text for post in importer.posts_by(['Joe Bloggs'])]
-        assert post_ids == ['post-1','post-3']
-
-    def test_posts_by_authors(self):
-        feed = AtomFeedFixture()
-        for author in ['Joe Bloggs','Steve Jobs','Not Joe Bloggs','Jane Smith','Bill Gates','John Smith','Bill Gates']:
-            feed.add_post(author=author)
-        importer = AtomImporter(str(feed))
-        post_ids = [post.id.text for post in importer.posts_by(['Steve Jobs', 'Bill Gates'])]
-        assert post_ids == ['post-2','post-5','post-7']
-
-    def test_comments_on_post(self):
-        feed = AtomFeedFixture()
-        post1 = feed.add_post()
-        comment1 = feed.add_comment(post1)
-        post2 = feed.add_post()
-        comment2 = feed.add_comment(post2)
-        comment3 = feed.add_comment(post1)
-        importer = AtomImporter(str(feed))
-        comment_ids = [comment.id.text for comment in importer.comments_on_post(post1)]
-        assert comment_ids == [comment1, comment3]
-
-
+class TestImporter(object):
     url_content = (
         ('Some text', 'Some text'),
         ('Blah <a href="http://www.overcomingbias.com/2007/11/passionately-wr.html">Link</a> more',
@@ -154,111 +102,36 @@ class TestAtomImporter(object):
             http://www.overcomingbias-rewritten.com/2006/11/beware_heritabl.html#comment-25685746"""),
         ('Unicode: (http://www.overcomingbias.com/ÜnîCöde¡っ)', 'Unicode: (http://www.overcomingbias-rewritten.com/ÜnîCöde¡っ)'),
     )
-    
+
     @staticmethod
     def url_rewriter(match):
         # This replacement will deliberately match again if the importer
-        # processes the same url twice 
+        # processes the same url twice
         return match.group().replace('overcomingbias', 'overcomingbias-rewritten')
-
-    def test_rewrite_urls_in_post_body(self):
-        for input_content, expected_content in self.url_content:
-            feed = AtomFeedFixture()
-            post = feed.add_post(content=input_content)
-            importer = AtomImporter(str(feed), url_handler=self.url_rewriter)
-            yield self.check_text, importer.get_post(post).content.text, expected_content
 
     @staticmethod
     def check_text(text, expected_text):
         assert text == expected_text
-    
-    def test_rewrite_urls_in_comments(self):
-        for input_content, expected_content in self.url_content:
-            feed = AtomFeedFixture()
-            post = feed.add_post()
-            feed.add_comment(post_id=post, content=input_content)
-            importer = AtomImporter(str(feed), url_handler=self.url_rewriter)
-            for comment in importer.comments_on_post(post):
-                yield self.check_text, comment.content.text, expected_content
 
-    pw_re = re.compile(r'[1-9a-hjkmnp-uwxzA-HJKMNP-UWXZ@#$%^&*]{8}')
     def test_generate_password(self):
-        
+        pw_re = re.compile(r'[1-9a-hjkmnp-uwxzA-HJKMNP-UWXZ@#$%^&*]{8}')
+
         # This test is a bit questionable given the random generation
         # but its better than no test
         for i in range(10):
             password = r2.lib.importer.generate_password()
             # print password
-            assert self.pw_re.match(password)
+            assert pw_re.match(password)
 
-
-    # def test_username_from_name(self):
-    #     feed = AtomFeedFixture()
-    #     post = feed.add_post()
-    #     importer = AtomImporter(str(feed))
-    #     
-    #     names = (
-    #         ('First Last', 'First_Last'), # Spaces
-    #         ('~O~','~O~'),
-    #         ('Øyvind','Øyvind'),
-    #         ('x-ray fluorescence','x-ray_fluorescence'),
-    #         ('"Q" the Enchanter', 'Q_the_Enchanter'),
-    #         ('(Hope)Fully Rational', 'HopeFully_Rational'),
-    #         ('-', '-'),
-    #         ('-ck-', '-ck-'),
-    #         ('-dan', '-dan'),
-    #         ('...', '...'),
-    #         ('.5%', '.5%'),
-    #         ('/ehj2', '/ehj2'),
-    #         ('01', '01'),
-    #         ('3.14159', '3.14159'),
-    #         ('4σ', '4σ'),
-    #         ('51a1fc26f78b0296a69f53c615ab5a2f64ab1d1e', '51a1fc26f78b0296a69f53c615ab5a2f64ab1d1e'),
-    #         ('?', '?'),
-    #         ('????a', '????a'),
-    #         ('A woman\'s POV', 'A woman\'s POV'),
-    #         ('A. Coward', 'A. Coward'),
-    #         ('A.B.', 'A.B.'),
-    #         ('A.S. (a hikikomori from Russia)', 'A.S. (a hikikomori from Russia)'),
-    #         ('AW', 'AW'),
-    #         ('Abdullah Khalid Siddiqi', 'Abdullah Khalid Siddiqi'),
-    #         ('Agent00yak', 'Agent00yak'),
-    #         ('Alexander Fiske-Harrison', 'Alexander Fiske-Harrison'),
-    #         ('Anon of /co/ and /tg/', 'Anon of /co/ and /tg/'),
-    #         ('Another somewhat sceptical statistician', 'Another somewhat sceptical statistician'),
-    #         ('Anónima', 'Anónima'),
-    #         ('Awww, a girlfriend > Singularity', 'Awww, a girlfriend > Singularity'),
-    #         ('Faré', 'Faré'),
-    #         ('Firozali A Mulla MBA PhD', 'Firozali A Mulla MBA PhD'),
-    #         ('Firozali A.Mulla  MBA PhD', 'Firozali A.Mulla  MBA PhD'),
-    #         ('Franklin D. Fields, Jr.', 'Franklin D. Fields, Jr.'),
-    #         ('Geebu$', 'Geebu$'),
-    #         ('Hmm...', 'Hmm...'),
-    #         ('Hoàng Đức Hiếu', 'Hoàng Đức Hiếu'),
-    #         ('Håkan Andersson', 'Håkan Andersson'),
-    #         ('MyMindOnMyMoney & MyMoneyOnMyMind', 'MyMindOnMyMoney & MyMoneyOnMyMind'),
-    #         ('Santa Claus (is my legal name)', 'Santa Claus (is my legal name)'),
-    #         ('Z Z z z z e x ...', 'Z Z z z z e x ...'),
-    #         ('_Felix', '_Felix'),
-    #         ('____', '____'),
-    #         ('jaltcoh.blogspot.com', 'jaltcoh.blogspot.com'),
-    #         ('spindizzy@eml.cc', 'spindizzy@eml.cc'),
-    #     )
-    #     for name, username in names:
-    #         yield self.check_text, importer._username_from_name(name), username
-        
-    def test_set_sort_order(self):
-        pass
-    
     def test_filter_html_in_titles(self):
         pass
-    
+
     def test_set_comment_is_html(self):
         pass
-    
+
     def test_auto_account_creation(self):
         pass
-    
+
     def test_cleaning_of_content(self):
         # There are a lot of ^M's in the comments
         pass
@@ -266,98 +139,129 @@ class TestAtomImporter(object):
 from mocktest import *
 from r2.models import Account
 from r2.models.account import AccountExists
-class TestAtomImporterMocktest(TestCase):
-    
+class TestImporterMocktest(TestCase):
+
     @property
     def importer(self):
-        feed = AtomFeedFixture()
-        post = feed.add_post()
-        feed.add_comment(post_id=post)
-
-        return AtomImporter(
-            str(feed),
-        )
+        return Importer()
 
     def test_get_or_create_account_exists(self):
-        anchor = mock_on(Account)
+        fixture = ImporterFixture()
+        post = fixture.add_post(author='Test User', author_email='user@host.com')
+
+        sr = mock_wrapper()
+        account_anchor = mock_on(Account)
         account = mock_wrapper().with_methods(_safe_load=None)
-        anchor._query.returning([account.mock]).is_expected
-        assert self.importer._get_or_create_account('Test User', 'user@host.com') == account.mock
+        account_anchor._query.returning([account.mock]).is_expected
+
+        mock_on(r2.lib.importer).register.is_expected.no_times()
+
+        self.importer.import_into_subreddit(sr.mock, fixture.get_data())
 
     def test_get_or_create_account_exists2(self):
-        account = mock_wrapper().with_methods(_safe_load=None)
+        fixture = ImporterFixture()
+        post = fixture.add_post(author='Test User', author_email='user@host.com')
+
         def query_action(name_match, email_match):
             return [account.mock] if name_match and email_match else []
-        
-        anchor = mock_on(Account)
-        query = anchor._query
+
+        sr = mock_wrapper()
+        account_anchor = mock_on(Account)
+        account = mock_wrapper().with_methods(_safe_load=None)
+        query = account_anchor._query
         query.action = query_action
         query.is_expected.twice() # Second attempt should succeed
-        anchor.c.with_children(name='TestUser', email='user@host.com')
+        account_anchor.c.with_children(name='TestUser', email='user@host.com')
 
-        assert self.importer._get_or_create_account('Test User', 'user@host.com') == account.mock
+        mock_on(r2.lib.importer).register.is_expected.no_times()
+
+        self.importer.import_into_subreddit(sr.mock, fixture.get_data())
 
     def test_get_or_create_account_exists3(self):
-        account = mock_wrapper().with_methods(_safe_load=None)
+        fixture = ImporterFixture()
+        post = fixture.add_post(author='Test User', author_email='user@host.com')
+
         def query_action(name_match, email_match):
             return [account.mock] if name_match and email_match else []
-        
-        anchor = mock_on(Account)
-        query = anchor._query
+
+        sr = mock_wrapper()
+        account_anchor = mock_on(Account)
+        account = mock_wrapper().with_methods(_safe_load=None)
+        query = account_anchor._query
         query.action = query_action
         query.is_expected.thrice() # Third attempt should succeed
-        anchor.c.with_children(name='Test_User', email='user@host.com')
+        account_anchor.c.with_children(name='Test_User', email='user@host.com')
 
-        assert self.importer._get_or_create_account('Test User', 'user@host.com') == account.mock
+        mock_on(r2.lib.importer).register.is_expected.no_times()
+
+        self.importer.import_into_subreddit(sr.mock, fixture.get_data())
 
     def test_get_or_create_account_not_exists(self):
         """Should create the account if it doesn't exist"""
-        account = mock_wrapper().with_methods(_safe_load=None)
-        anchor = mock_on(Account)
-        query = anchor._query
+        fixture = ImporterFixture()
+        post = fixture.add_post(author='Test User', author_email='user@host.com')
+
+        sr = mock_wrapper()
+        account_anchor = mock_on(Account)
+        query = account_anchor._query
         query.return_value = []
         query.is_expected.thrice()
 
         test_user5 = mock_wrapper().with_methods(_safe_load=None)
         test_user5.name = 'Test_User5'
-        
-        def register_action(name, pw, email):
+
+        def register_action(name, password, email):
             if name != test_user5.name:
                 raise AccountExists
             else:
                 return test_user5.mock
 
         # Mocking on importer because it imported register
-        account_module = mock_on(r2.lib.importer)
-        register = account_module.register
+        account_module_anchor = mock_on(r2.lib.importer)
+        register = account_module_anchor.register
         register.is_expected.exactly(4).times
         register.action = register_action
 
-        created_account = self.importer._get_or_create_account('Test User', 'user@host.com')
-        assert str(created_account) == 'Test_User5'
+        self.importer.import_into_subreddit(sr.mock, fixture.get_data())
 
     def test_get_or_create_account_max_retries(self):
         """Should raise an error after 10 tries"""
-        anchor = mock_on(Account)
-        query = anchor._query.returning([]).is_expected.thrice()
-        account_module = mock_on(r2.lib.importer)
-        register = account_module.register.raising(AccountExists).is_expected.exactly(10).times
+        fixture = ImporterFixture()
+        post = fixture.add_post(author='Test User', author_email='user@host.com')
 
+        sr = mock_wrapper()
+        account_anchor = mock_on(Account)
+        query = account_anchor._query.returning([]).is_expected.thrice()
+        account_module_anchor = mock_on(r2.lib.importer)
+        register = account_module_anchor.register.raising(AccountExists).is_expected.exactly(10).times
+
+        self.importer.import_into_subreddit(sr.mock, fixture.get_data())
+
+    @ignore
+    def test_failing_post(self):
         self.assertRaises(
             StandardError, lambda: self.importer._get_or_create_account('Test User', 'user@host.com'),
             message='Unable to generate account after 10 retries')
 
-    @pending
+    @ignore
     def test_import_into_subreddit(self):
         sr = mock_wrapper()
 
-        feed = AtomFeedFixture()
-        post = feed.add_post()
-        feed.add_comment(post_id=post)
+        fixture = ImporterFixture()
+        post = fixture.add_post()
+        fixture.add_comment(post_id=post)
 
-        importer = AtomImporter(str(feed))
-        importer.import_into_subreddit(sr)
+        importer = Importer()
+        importer.import_into_subreddit(sr, fixture.get_data())
 
-# class TestRewitingOvercomngBiasUrls(TestCase):
-#     def test_x(self):
-#         
+    @pending
+    def test_set_sort_order(self):
+        fixture = ImporterFixture()
+        post = fixture.add_post()
+        fixture.add_comment(post)
+        importer = Importer()
+        sr = mock_wrapper()
+        link = mock_wrapper()
+        mock_link = mock_on(Link)
+        mock_link.create.returning(link.mock).with_args().is_expected
+        importer.import_into_subreddit(sr.mock, fixture.get_data())
