743102c6b222f7b3f31132ded3459a72f36d6887
Wesley Moore <wes@trike.com.au>
2009-04-01 15:47:19 +1100

    Set default_sr to true in Pylons context

--- scripts/ob_import_run.py
+++ scripts/ob_import_run.py
@@ -1,9 +1,10 @@
 import yaml
 from r2.models import Subreddit
 from r2.lib.importer import Importer
-
+import pylons
 
 def import_posts(filename, sr_name):
+    pylons.c.default_sr = True
     sr = Subreddit._by_name(sr_name)
 
     data = yaml.load(open(filename), Loader=yaml.CLoader)
