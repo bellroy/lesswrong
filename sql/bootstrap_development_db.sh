f6dbdbd9881ff68da74af9010f3e483eaf065c6f
Laurent Bossavit <laurent@BigMac.local>
2010-03-24 21:15:22 +0100

    Added construction of test DBs and stored procs to bootstrap script.

--- sql/bootstrap_development_db.sh
+++ sql/bootstrap_development_db.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dbs="reddit changes email query_queue"
+dbs="reddit changes email query_queue reddit_test changes_test email_test query_queue_test"
 
 for db in $dbs; do
     sudo -u postgres dropdb $db
@@ -8,4 +8,4 @@ for db in $dbs; do
 done
 
 psql -U reddit -W < sql/functions.sql
-
+psql -U reddit -d reddit_test -W < sql/functions.sql
