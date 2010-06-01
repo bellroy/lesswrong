93bd3b254c0b5671875901f035029ac51d49d756
Wesley Moore <wes@trike.com.au>
2010-06-01 11:51:33 +1000

    Confirm manual test script has been run when deploying

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,7 @@ set :stages, stages
 require 'capistrano/ext/multistage'
 load 'config/cap-tasks/trike-tasks.rb'
 load 'config/cap-tasks/git.rb'
+load 'config/cap-tasks/test.rb'
 load 'config/db.rb'
 
 set :scm, 'git'
