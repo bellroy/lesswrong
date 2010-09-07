46e173f88bfd749765a7925af69ea8639df65db2
Wesley Moore <wes@trike.com.au>
2010-09-07 14:55:12 +1000

    Update staging cap configuration to use AWS

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,10 +1,22 @@
-set :application, "lesswrong.org"
-set :domains, %w[ lesswrong.org ]
-set :deploy_to, "/srv/www/#{application}"
-set :rails_env, "NA"
-set :branch, 'staging'
+load 'config/cap-tasks/trike-aws.rb'
 
-role :app, "polly.trike.com.au"
-role :web, "polly.trike.com.au"
-role :db,  "polly.trike.com.au", :primary => true
+set :application, "lesswrong.com"
+set :domains, %w[ lesswrong.com ]
+set :deploy_to, "/srv/www/#{application}"
+set :branch, 'staging'
+set :environment, 'staging'
+
+set :instance, lambda {
+  AWS.find_or_start_host_for_security_group(
+    'webserver_python_staging',
+    'ami-4446ac2d',
+    #AWS.auto_scaler_ami('python'),
+    120,
+    File.join('config', "user_data_#{environment}.sh.erb")
+  )
+}
+
+role :app, instance, :primary => true
+role :web, instance, :primary => true
+role :db,  "db.aws.trike.com.au", :primary => true, :no_release => true
 
