5e17e65eb8d5a5de6e781ffc2bb84a062ee2778a
Wesley Moore <wes@trike.com.au>
2010-09-21 16:50:44 +1000

    Fix header property name in configure discussion

--- scripts/configure_discussion_subreddit.py
+++ scripts/configure_discussion_subreddit.py
@@ -3,7 +3,7 @@
 def configure_discussion():
   from r2.models import Subreddit
   s = Subreddit._by_name('discussion')
-  s.header_image = "/static/logo-discussion.png"
+  s.header = "/static/logo-discussion.png"
   s.stylesheet = "/static/discussion.css"
   s.infotext = u"""This part of the site is for the discussion of topics not
                    yet ready or not suitable for normal top-level posts.
