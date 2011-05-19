67946eb3622455c53225be01f415ce0f8e302dce
Wesley Moore <wes@trikeapps.com>
2011-05-19 13:49:56 +1000

    Add datetime method to meetup

--- r2/r2/models/meetup.py
+++ r2/r2/models/meetup.py
@@ -1,4 +1,11 @@
 from r2.lib.db.thing import Thing
 
+import pytz
+from datetime import datetime
+from r2.lib.utils import FixedOffset
+
 class Meetup(Thing):
-  pass
+  def datetime(self):
+    utc_timestamp = datetime.fromtimestamp(self.timestamp, pytz.timezone('utc'))
+    tz = FixedOffset(self.tzoffset, None)
+    return utc_timestamp.astimezone( tz )
