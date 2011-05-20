b63014c402153a430654ca6979afb5f18385e8e2
Wesley Moore <wes@trikeapps.com>
2011-05-20 14:45:50 +1000

    Add meetups listing page

--- r2/r2/models/meetup.py
+++ r2/r2/models/meetup.py
@@ -9,3 +9,14 @@ class Meetup(Thing):
     utc_timestamp = datetime.fromtimestamp(self.timestamp, pytz.utc)
     tz = FixedOffset(self.tzoffset, None)
     return utc_timestamp.astimezone(tz)
+
+  @classmethod
+  def add_props(cls, user, items):
+    pass
+
+  def keep_item(self, item):
+    return True
+
+  @staticmethod
+  def cache_key(item):
+    return False
