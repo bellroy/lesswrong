0286fb996e4817175e6a5e3919d60c93dff4cfdd
Wesley Moore <wes@trikeapps.com>
2011-05-23 14:35:51 +1000

    Add distance related methods to Meetup

--- r2/r2/models/meetup.py
+++ r2/r2/models/meetup.py
@@ -5,6 +5,7 @@ import time
 from datetime import datetime
 from r2.lib.utils import FixedOffset
 from r2.lib.db.operators import desc
+from geolocator import gislib
 
 class Meetup(Thing):
   def datetime(self):
@@ -27,6 +28,25 @@ class Meetup(Thing):
     # Sort in Python because the Thing API only provides sorting by base columns
     meetups.sort(key=lambda m: m.timestamp)
     return meetups
+
+  @classmethod
+  def upcoming_meetups_near(cls, point):
+    meetups = list(cls.upcoming_meetups_query())
+
+    # Find nearby ones
+    meetups = filter(lambda m: m.distance_to(point) < 100, meetups)
+
+    # Sort in Python because the Thing API only provides sorting by base columns
+    #meetups.sort(key=lambda m: )
+    return meetups
+
+
+  def distance_to(self, location):
+    """
+    Returns the distance from this meetup to the passed point. The point is
+    tuple, (lat, lng)
+    """
+    return gislib.getDistance((self.latitude, self.longitude), location)
 
   def keep_item(self, item):
     return True
