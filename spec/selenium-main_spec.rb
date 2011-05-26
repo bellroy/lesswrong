91e95c41651ded85111980c1575c100b98893068
David Powell <david@drp.id.au>
2011-05-26 14:20:34 +1000

    Remove my firefox specific override from selenium tests

--- spec/selenium-main_spec.rb
+++ spec/selenium-main_spec.rb
@@ -7,12 +7,6 @@ RSpec.configure do |config|
   config.include Capybara
 end
 
-# Run firefox3 cause firefox4 is buggy on macosx 10.5
-require 'selenium-webdriver'
-#Selenium::WebDriver::Firefox.path= '/Applications/Firefox3.app/Contents/MacOS/firefox-bin'
-Selenium::WebDriver::Firefox.path= '/Users/dave/firefox.sh'
-
-
 # NOTE: there seems to be a feature(?) in firefox that will only deliver certain js events
 # if the browser window has focus.  Thus, some of these tests will only work if the main
 # window has focus.
