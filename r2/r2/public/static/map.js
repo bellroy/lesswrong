103222b3cbdfbf53340e28cf59b9861069cd1f6a
David Powell <david@drp.id.au>
2011-07-01 13:03:42 +1000

    Zoom front page map to all markers

--- r2/r2/public/static/map.js
+++ r2/r2/public/static/map.js
@@ -4,22 +4,22 @@
     if (map) {
       loadMaps(function() { 
         var markers = $('.marker', map);
-        var cntrData = markers.first();
-        var lat = cntrData.attr('data-latitude');
-        var lng = cntrData.attr('data-longitude');
-        var cntr = new google.maps.LatLng(lat,lng);
         var myOptions = {
-          zoom: 16,
-          center: cntr,
+          zoom: 12,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-        
         var gMap = new google.maps.Map(map, myOptions);
+
+        var first;
+        var bounds = new google.maps.LatLngBounds();
 
         markers.each(function(i,m) {
           var lat = m.readAttribute('data-latitude');
           var lng = m.readAttribute('data-longitude');
           latlng = new google.maps.LatLng(lat, lng);
+          bounds.extend(latlng);
+          if (!first)
+            first = latlng;
           new google.maps.Marker({
             map: gMap,
             draggable: false,
@@ -28,6 +28,13 @@
             title: m.readAttribute('data-title')
           });
         });
+
+        /* Show all markers, and center on the first */
+        if (bounds.getNorthEast().lat() != bounds.getSouthWest().lat() ||
+            bounds.getNorthEast().lng() != bounds.getSouthWest().lng())
+          gMap.fitBounds(bounds);
+        if (first)
+          gMap.setCenter(first);
       });
     }
   };
