59b0bf3e16581d73ece903a44263bc57278c96ee
Wesley Moore <wes@trikeapps.com>
2012-06-05 14:08:11 +1000

    Change cap config to work with new Chef managed server

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,34 +1,62 @@
+load 'config/cap-tasks/common.rb'
+load 'config/cap-tasks/test.rb'
+load 'config/cap-tasks/console.rb'
+load 'config/cap-tasks/rake.rb'
+load 'config/cap-tasks/secrets.rb'
+load 'config/cap-tasks/assets.rb'
+load 'config/cap-tasks/postgresql_dump.rb'
+load 'config/db.rb'
+
 stages_glob = File.join(File.dirname(__FILE__), "deploy", "*.rb")
 stages = Dir[stages_glob].collect { |f| File.basename(f, ".rb") }.sort
 set :stages, stages
 
 require 'capistrano/ext/multistage'
-load 'config/cap-tasks/common.rb'
-load 'config/cap-tasks/test.rb'
-load 'config/cap-tasks/console.rb'
-load 'config/cap-tasks/console.rb'
-load 'config/cap-tasks/rake.rb'
-load 'config/cap-tasks/postgresql_dump.rb'
-load 'config/db.rb'
+require 'bundler/capistrano'
 
-set :scm, 'git'
-set :repository, "git@github.com:tricycle/lesswrong.git"
+set :application, "lesswrong"
+# This must be passed as a block, since environment is defined in the individual
+# stages later.
+set(:user) { "#{application}-#{environment}" }
+set :scm, "git"
+set :repository, 'git@github.com:tricycle/lesswrong.git'
+set :repository_cache, 'cached-copy'
 set :git_enable_submodules, 1
 set :deploy_via, :remote_cache
-set :repository_cache, 'cached-copy'
+set :branch, "stable" # Default branch
 set :engine, "paster"
-
-# Be sure to change these in your application-specific files
-set :branch, 'stable'
-set :rails_env, nil
-set :user, "www-data"            # defaults to the currently logged in user
 set :public_path, lambda { "#{current_path}/r2/r2/public" }
 set :databases, %w[main change email query_queue]
+set(:rails_env) { environment } # Used for compatibility with shared cap tasks that assume its presence
+
+# This must be passed as a block, since environment is defined in the individual
+# stages later.
+set(:deploy_to) { "/srv/www/#{application}-#{environment}" }
+
+# Secrets
+set :secrets_repository, "git@git.trikeapps.com:settings/lesswrong.git"
+set(:symlinked_configs) { [ "#{environment}.ini" ] }
+set :bundle_without, [:development, :test]
+
+#set :bundle_without, [:development, :test, :cucumber]
+
+before 'deploy:update_code', 'git:ensure_pushed'
+
+after 'deploy:update_code',  "secrets:update_configs",
+                             "assets:symlink_shared_dirs",
+                             "deploy:rake_after_update_code"
 
 namespace :deploy do
-  after "deploy:update_code", :roles => [:web, :app] do
-    %w[files assets].each {|dir| link_shared_dir(dir) }
+  desc "Create a symlink in the release path to all _symlinked_configs_"
+  task :symlink_configs do
+    symlinked_configs.each do |file|
+      run "ln -sf #{shared_path}/config/#{file} #{release_path}/config/#{file}"
+    end
   end
+
+  #after "deploy:update_code", :roles => [:web, :app] do
+  #  %w[files assets].each {|dir| link_shared_dir(dir) }
+  #end
 
   def rake_options
     {
@@ -63,6 +91,5 @@ namespace :deploy do
   end
 end
 
-before 'deploy:update_code', 'git:ensure_pushed'
-after "deploy:update_code", "deploy:rake_after_update_code"
+
 
