e87fd8e418c83cb45c3230d54f781247c57b3b81
John Simon <john@johnsoft.com>
2012-09-08 08:44:54 +0200

    Add spinner when voting on polls

--- r2/r2/public/static/polls.js
+++ r2/r2/public/static/polls.js
@@ -3,6 +3,7 @@ var Poll = {};
 Poll.submitBallot = function (form) {
     var ownerThing = Thing.getThingRow(form);
     var ownerThingID = _id(ownerThing);
+    var votingArea = jQuery(form).find(".poll-voting-area");
 
     if (!logged) {
         showcover(true, "vote_" + ownerThingID);
@@ -12,14 +13,15 @@ Poll.submitBallot = function (form) {
     var data = {uh: modhash, owner_thing: ownerThingID};
 
     for(var i = 0; i < form.elements.length; i++) {
-        if(! form.elements[i].id || !id || 
-           _id(form.elements[i]) == id) {
-            var f = field(form.elements[i]);
-            if (f) {
-                data[form.elements[i].name] = f;
-            }
-        }
+        var value = field(form.elements[i]);
+        if (value)
+            data[form.elements[i].name] = value;
     }
 
-    result = redditRequest('submitballot', data, null, true);
+    function done() {
+        votingArea.removeClass("inprogress");
+    }
+
+    votingArea.addClass("inprogress");
+    result = redditRequest("submitballot", data, null, true, {cleanup_func: done});
 };
