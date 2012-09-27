cde5bd1aefb07ec89525f6d38b689b106aca74b2
John Simon <john@johnsoft.com>
2012-09-26 20:58:30 -0400

    Extract the geocoded input box out to a reusable function

--- r2/r2/public/static/meetups.js
+++ r2/r2/public/static/meetups.js
@@ -1,30 +1,22 @@
-(function() {
-
-  document.observe("dom:loaded", function() {
-    createMap( $('map') );
-  });
-
-  /* Add Meetup */
-  function setTimeZone(date) {
-    $$('input[name="tzoffset"]').each(function(el) {
-      var tz = (new Date()).getTimezoneOffset()
-      el.setValue(tz / -60)
-    });
-  }
+function makeGeocodedInputWidget(options) {
+  var inputElement = options.input;
+  var iconElement = jQuery('<img id="geocoded_status" src="/static/spinner.gif"' +
+                             ' alt="status" style="display:none">')[0];
+  var messageElement = jQuery('<div id="geocoded_location">')[0];
+  jQuery(inputElement).after(messageElement).after(iconElement);
 
   var statusIcons = {
     "spinner": "/static/spinner.gif",
-    "ok": "/static/accept.png",
-    "error": "/static/exclamation.png"
+    "ok":      "/static/accept.png",
+    "error":   "/static/exclamation.png"
   };
 
   function updateGeocodeStatus(status, message) {
-    var el = $('geocode_status');
-    el.writeAttribute('src', statusIcons[status]);
-    el.show();
+    iconElement.writeAttribute("src", statusIcons[status]);
+    iconElement.show();
 
     if (message) {
-      $('geocoded_location').update(message);
+      messageElement.update(message);
     }
   }
 
@@ -42,12 +34,28 @@
         var result = results.first();
         var location = result.geometry.location;
         updateGeocodeStatus('ok', result.formatted_address);
-        $$('input[name="latitude"]').first().setValue(location.lat());
-        $$('input[name="longitude"]').first().setValue(location.lng());
-      }
-      else {
+        jQuery(options.latitude).val(location.lat());
+        jQuery(options.longitude).val(location.lng());
+      } else {
         updateGeocodeStatus('error');
       }
+    });
+  }
+
+  loadMaps(function() {
+    inputElement.observe('change', geocodeLocation);
+  });
+}
+
+(function() {
+  document.observe("dom:loaded", function() {
+    createMap( $('map') );
+  });
+
+  function setTimeZone(date) {
+    $$('input[name="tzoffset"]').each(function(el) {
+      var tz = (new Date()).getTimezoneOffset()
+      el.setValue(tz / -60)
     });
   }
 
@@ -56,12 +64,13 @@
     if (form) {
       form.focusFirstElement();
 
-      loadMaps(function() {
-        $('location').observe('change', geocodeLocation);
-      });
-
-      /* Fill in the current time zone */
       setTimeZone();
+
+      makeGeocodedInputWidget({
+        input:    $('location'),
+        latitude: $$('input[name="latitude"]').first(),
+        latitude: $$('input[name="longitude"]').first()
+      });
 
       Protoplasm.use('timepicker', function() { /* Used by datepicker below */
         Protoplasm.use('datepicker', function() {
