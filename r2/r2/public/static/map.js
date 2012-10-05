c6f55e99639a651b537431344bc55d27f349d2d9
John Simon <john@johnsoft.com>
2012-10-05 01:18:29 -0400

    Don't surround entire function in if block

--- r2/r2/public/static/map.js
+++ r2/r2/public/static/map.js
@@ -1,48 +1,49 @@
 (function ($) {
   /* Show Meetup map */
   window.createMap = function (map) {
-    if (map) {
-      loadMaps(function() { 
-        var markers = $('.marker', map);
-        var myOptions = {
-          zoom: 12,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var gMap = new google.maps.Map(map, myOptions);
+    if (!map)
+      return;
 
-        var first;
-        var bounds = new google.maps.LatLngBounds();
+    loadMaps(function() { 
+      var markers = $('.marker', map);
+      var myOptions = {
+        zoom: 12,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      var gMap = new google.maps.Map(map, myOptions);
 
-        markers.each(function(i,m) {
-          var lat = $(m).attr('data-latitude');
-          var lng = $(m).attr('data-longitude');
-          var latlng = new google.maps.LatLng(lat, lng);
-          bounds.extend(latlng);
-          if (!first)
-            first = latlng;
-          var marker = new google.maps.Marker({
-            map: gMap,
-            draggable: false,
-            animation: google.maps.Animation.DROP,
-            position: latlng,
-            title: $(m).attr('data-title')
-          });
-          var url = $(m).attr('data-url');
-          if (url) {
-            google.maps.event.addListener(marker, 'click', function() {
-              window.location.href = url;
-            });
-          }
+      var first;
+      var bounds = new google.maps.LatLngBounds();
+
+      markers.each(function(i,m) {
+        var lat = $(m).attr('data-latitude');
+        var lng = $(m).attr('data-longitude');
+        var latlng = new google.maps.LatLng(lat, lng);
+        bounds.extend(latlng);
+        if (!first)
+          first = latlng;
+        var marker = new google.maps.Marker({
+          map: gMap,
+          draggable: false,
+          animation: google.maps.Animation.DROP,
+          position: latlng,
+          title: $(m).attr('data-title')
         });
-
-        /* Show all markers, and center on the first */
-        if (bounds.getNorthEast().lat() !== bounds.getSouthWest().lat() ||
-            bounds.getNorthEast().lng() !== bounds.getSouthWest().lng())
-          gMap.fitBounds(bounds);
-        if (first)
-          gMap.setCenter(first);
+        var url = $(m).attr('data-url');
+        if (url) {
+          google.maps.event.addListener(marker, 'click', function() {
+            window.location.href = url;
+          });
+        }
       });
-    }
+
+      /* Show all markers, and center on the first */
+      if (bounds.getNorthEast().lat() !== bounds.getSouthWest().lat() ||
+          bounds.getNorthEast().lng() !== bounds.getSouthWest().lng())
+        gMap.fitBounds(bounds);
+      if (first)
+        gMap.setCenter(first);
+    });
   };
 
   window.makeGeocodedInputWidget = function (options) {
