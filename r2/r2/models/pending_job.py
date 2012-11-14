ab84a39502f80bea1f8dc1cbba5815a3a2a8225c
Wesley Moore <wes@trikeapps.com>
2012-11-15 10:25:02 +1100

    Add defaults to PendingJob
    To ensure that run_at and datta attributes are always accessible.
    
    [#39587297]

--- r2/r2/models/pending_job.py
+++ r2/r2/models/pending_job.py
@@ -2,6 +2,9 @@ from r2.lib.db.thing import Thing
 
 
 class PendingJob(Thing):
+    _defaults = dict(run_at = None,
+                     data   = None)
+
     @classmethod
     def store(cls, run_at, action, data=None):
         adjustment = cls(run_at=run_at, action=action, data=data)
