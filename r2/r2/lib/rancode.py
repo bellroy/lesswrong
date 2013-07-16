2d8c5a4ee1b0bcd8ded5e6daf21f6e2d80a27df0
Wesley Moore <wes@trikeapps.com>
2013-07-16 16:16:19 +1000

    Simplify random email verification code generator

--- r2/r2/lib/rancode.py
+++ r2/r2/lib/rancode.py
@@ -3,7 +3,4 @@ import string
  
 # Returns a random alphanumeric string of length 'length'
 def random_key(length):
-    key = ''
-    for i in range(length):
-        key += random.choice(string.uppercase + string.digits)
-    return key
+    return ''.join(random.sample(string.uppercase + string.digits, length))
